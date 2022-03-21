0fbee437f060a51622df8f7784c6fc395cc6809f
Courtney Ficker <cficker@pdx.edu>
2022-03-21 04:59:21 -0700

    Queries now return dictionaries, much work remains on displaying results in embeds

--- commands/Minis.py
+++ commands/Minis.py
@@ -1,10 +1,41 @@
+from unicodedata import numeric
 import discord
 from discord.ext import commands
+import mysql.connector
+from mysql.connector import errorcode
+from os import getenv as env
+
+config = {
+  'user': env("DB_USER"),
+  'password': env("DB_PW"),
+  'host': env("DB_HOST"),
+  'database': env("DB_NAME"),
+  'raise_on_warnings': True
+}
+
+db = None
+cursor = None
+
+def testConnection():
+    try:
+        db = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Username or password invalid!")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist!")
+        else:
+            print(err)
+        return False
+    else:
+        db.close()
+        return True
 
 class Minis(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        
 
     @commands.command()
     async def search(self, ctx, *args):
@@ -15,8 +46,40 @@ class Minis(commands.Cog):
             .search Gold Dragon
         '''
 
-        term = ' '.join(args)
-        await ctx.send(f'Searching mini database for {term}...')
+        if testConnection():
+            db = mysql.connector.connect(**config)
+            cursor = db.cursor(dictionary=True)
+        else:
+            print("Error connecting to DB!")
+            await ctx.send("Error connecting to DB!")
+
+        term = ("%" + ' '.join(args) + "%",)
+        query = "SELECT id, name FROM minis WHERE name LIKE %s LIMIT 10;"
+    
+        
+        
+        cursor.execute(query,term)
+
+        result = cursor.fetchall()
+        print(result)
+        for r in result:
+            print(' -> ' + str(r))
+
+        print(cursor.statement)
+
+        # embed = discord.Embed(
+        #     title = 'Search Results',
+        #     description = 'This is what I have discovered in the archives...',
+        #     color = discord.Colour.dark_magenta()
+        # )
+        # for r in result:
+        #     embed.add_field(
+        #         name = r[1],
+        #         value = f'ID #{r[0]}',
+        #         inline=False
+        #     )
+        # await ctx.send(embed=embed)
+        await ctx.send('Hi...')
 
 
     @commands.command()
@@ -28,22 +91,68 @@ class Minis(commands.Cog):
         Usage:
             .mini 1138
         '''
+
+        if testConnection():
+            db = mysql.connector.connect(**config)
+            cursor = db.cursor(dictionary=True)
+        else:
+            print("Error connecting to DB!")
+            await ctx.send("Error connecting to DB!")
+
+        # Make sure argument is an integer
+        if not arg.isdigit():
+            await ctx.send("I apologize...I do not understand your request. ```md\nUsage: .mini 1234\n```")
+            return
+
+        query = '''SELECT
+                    minis.id,
+                    minis.qty,
+                    minis.name,
+                    minis.num,
+                    miniSet.total,
+                    miniSet.name AS setname,
+                    miniRarity.rarity,
+                    miniSize.size,
+                    miniRace.race
+                FROM
+                    minis,
+                    miniSet,
+                    miniRarity,
+                    miniSize,
+                    miniRace
+                WHERE
+                    minis.fSetID = miniSet.id AND
+                    minis.fRarityID = miniRarity.id AND
+                    minis.fSizeID = miniSize.id AND
+                    minis.fRaceID = miniRace.id AND
+                    minis.id = %s;
+                    '''
+
+        # query = "SELECT * FROM minis WHERE id = %s;"
+        cursor.execute(query,(arg,))
+        result = cursor.fetchall()
+        print(result)
+
+        # Remove list wrapper from dict
+        res = result[0]
+
         embed = discord.Embed(
-            title = 'Mini Information',
-            description = 'Info here info here info here info here info here info here',
+            title = res['name'],
+            # title = 'name here',
+            description = 'Mini Infomation',
             color = discord.Colour.dark_blue()
         )
         embed.add_field(
-            name = 'Name',
-            value='Drizzt'
+            name  = 'Name',
+            value = 'Drizzt'
         )
         embed.add_field(
-            name = 'Set',
-            value='Icons of the Realms'
+            name  = 'Set',
+            value = res['setname']
         )
         embed.add_field(
-            name = 'Owned',
-            value=1
+            name  = 'Owned',
+            value = res['qty']
         )
         await ctx.send(embed=embed)
 
